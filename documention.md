97b3e23666a128ce2022f8c1ccbf96315cf09a25
Debora Jaimes <deboranjaimes@gmail.com>
2022-02-23 09:50:42 -0400

    Commit Initial API Lista de Compras SoftList

---
+++ documention.md
@@ -0,0 +1,24 @@
+Projeto SoftLists
+
+1 - Objetivo
+    - Desenvolver um software Organizar as compras mensal
+
+2 - Telas
+    -  Tela de cadastro do Produto
+    -  Tela de cadastro de listagem
+    -  Tela de Login 
+
+3 - Funcionalidades
+    - Listagem de compras
+    - Cadatro de Item de compra
+    - Riscar itens quando a Flag Carrinho estiver marcado e 
+
+4 - Entidades
+- Usuário
+    - Nome
+    - Email
+    - Senha
+- Item de compra
+    - Descrição
+    - Quantidade
+    - Carrinho
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const res = require("express/lib/response")
+
+const api = express() //Cria a API
+const PORT = 3001
+
+
+api.use(cors()) //libera o acesso api
+api.use(express.json()) //faz as requisições do JSON sejam reconhecido
+
+//Função de conexão com o DB
+async function connect() {
+
+
+    await mongoose.connect("mongodb+srv://sa:sbdpu2001@cluster0.b6trq.mongodb.net/softList?retryWrites=true&w=majority")
+    console.log("BD conectado com sucesso")
+}
+
+connect() //Executa a Função de conexão DB
+
+const usuarioSchema = new mongoose.Schema({ // Entidade usuários
+    usuario: String,
+    email: String,
+    senha: String
+
+})
+
+const usuarioModel = mongoose.model("Usuario", usuarioSchema)
+
+
+const itemSchema = new mongoose.Schema({ //defina o Schema do Item
+    descricao: String,
+    quantidade: Number,
+    valorUnitario: Number,
+    valorTotal: Number,
+    carrinho: Boolean
+})
+
+const itemModel = mongoose.model("Item", itemSchema) //registra o model do Item
+
+api.get("/", (req, res) => {
+    res.json({ mensagem: "API SofList" })
+})
+    
+
+api.get("/item", async (req, res) => {
+    res.json(await itemModel.find({}))
+})
+
+api.post("/item", async (req,res) => {
+   const resultado = await itemModel.create(req.body)
+    res.json({mensagem: "Item adicionado com sucesso!"})
+})
+
+api.get("/usuario", async (req, res) => {
+    res.json(await usuarioModel.find({}))
+})
+
+api.post("/usuario", async (req, res) => {
+    res.json(await usuarioModel.create(req.body))
+})
+
+
+
+api.listen(PORT, () => {
+    console.log("PAI TA ONNNN E ROTEANDO!!!! IXXXQUECE TAMO IXXXXTORADO")
+})
+
